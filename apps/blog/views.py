709a8a1305d444b546e346e004062cf69c8475ee
Бойбородов Илья <fflash08@mail.ru>
2026-08-31 10:42:00 +0300

    Добавил модель постов, прописал маршруты и активировал

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Переносим функцию сюда
+
+
 def home_view(request):
     return HttpResponse("Главная страница из файла views.py")
+
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'index.html', {'posts': posts})
